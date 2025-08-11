c34fa2783d3a9c4204ddd0398c66562ae33aeeef
Vivek Rohtasvi <82621421+Vrohs@users.noreply.github.com>
2025-08-11 19:20:03 +0530

    add controls

--- vehicle.js
+++ vehicle.js
@@ -1,17 +1,19 @@
-class Car{
-    constructor(x_cord, y_cord, height, width){
+class Car {
+    constructor(x_cord, y_cord, height, width) {
         this.x_cord = x_cord;
         this.y_cord = y_cord;
         this.height = height;
         this.width = width;
+
+        this.controls = new Controls()
     }
 
-    draw(ctx){
+    draw(ctx) {
         ctx.beginPath()
-
+        ctx.fillStyle = 'red'
         ctx.rect(
-            this.x_cord - this.width/2,
-            this.y_cord - this.height/2,
+            this.x_cord - this.width / 2,
+            this.y_cord - this.height / 2,
             this.width,
             this.height
         );
